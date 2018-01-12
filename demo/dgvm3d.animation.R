b7b9cba6937c6da41f1bc415637e3bb6a758f1d6
Joerg Steinkamp <joergsteinkamp@yahoo.de>
2018-01-12 11:29:45 +0100

    Added some comments; slowed and shortened the animation demo

--- demo/dgvm3d.animation.R
+++ demo/dgvm3d.animation.R
@@ -1,48 +1,70 @@
 library(DGVM3D)
 
-rpm <- 3       #  rounds per minute
-fps <- 10      # frames per second
-duration <- 20 # seconds
+## pictures per year
+ppy <- 20
+## angle per year
+apy = 0.5
 
-## md <- file.path(tempdir(), "movie")
-## dir.create(md)
+## create a temporary directory
+md <- file.path(tempdir(), "movie")
+dir.create(md)
 
+## how many years should be visualized
 ystart <- 1860
-yend   <- 1959
+yend   <- 2005
 location <-  "Chile - temperate/mediterranean forest"
 
-dummy <- open3d(windowRect=c(0, 0, 800, 600), scale=c(1, 1, 1), FOV=1)
-stand <- snapshot(dgvm3d.succession[[location]],  year = ystart)
-rgl.clear("lights")
-rgl.light( theta = -25, phi = 30, specular = "black", diffuse = "#FFFFFF")
+## initialize the window and the stand
+dummy  <- open3d(windowRect=c(0, 0, 800, 600), scale=c(1, 1, 1), FOV=1)
+stand  <- snapshot(dgvm3d.succession[[location]],  year=ystart)
+nsoillayer <- sum(sapply(stand@patches, function(x) {length(x@soil) - 1}))
 
+## set the lights to be not so reflective
+rgl.clear("lights")
+rgl.light( theta=-25, phi=30, specular="black", diffuse="#FFFFFF")
+
+## do not change the bounding box and therefore the center of view.
+## Sadly the viewpoint cannot be set with rgl, only the observer position with observer3d
+par3d(ignoreExtent=TRUE)
+
+## view a bit more from the top
 rot.y = rotationMatrix(-pi / 3, 1, 0, 0)
-for (i in 0:50) {
-  rot.z = rotationMatrix(i * pi/360, 0, 0, 1)
+for (i in 0:ppy) {
+  ## start the rotation around the z-axis
+  rot.z = rotationMatrix(i * pi * apy / 360, 0, 0, 1)
   rgl.viewpoint(userMatrix = rot.y %*% rot.z, fov=1, zoom=0.75)
-  ## fname = sprintf("movie%08d.png", i)
-  ## rgl.snapshot(file.path(md, fname))
-  ## system(paste0("mogrify -fill black -pointsize 36 -annotate +50+68 '", location, " ", ystart, "' ", file.path(md, fname)))
+  ## save an image and add a label
+  fname = sprintf("movie%08d.png", i)
+  rgl.snapshot(file.path(md, fname))
+  system(paste0("mogrify -fill black -pointsize 36 -annotate +50+68 '", location, " ", ystart, "' ", file.path(md, fname)))
 }
 
+## continue with the next years
 for (y in seq(ystart + 1, yend, 1)) {
+  ## remove killed trees and establish new ones
   stand = updateStand(stand, dgvm3d.succession[[location]], y)
-  clear3d()
-  stand3D(stand)
+  ## remove the old vegetation
+  if (nrow(rgl.ids()) > (nsoillayer))
+    rgl.pop(id=rgl.ids()[(nsoillayer + 1):nrow(rgl.ids()), "id"])
+  #stand3D(stand)
+  ## first calculate image, then draw it. Speeds up everything by several orders of magnitude
   par3d(skipRedraw=TRUE)
   stand = plant3D(stand)
   fire3D(stand, limit=0.2)
   par3d(skipRedraw=FALSE)
 
-  for (i in 1:50) {
-    rot.z = rotationMatrix(((y - ystart) * 50 + i) * pi / 360, 0, 0, 1)
+  ## continue rotation around the z-axis
+  for (i in 1:ppy) {
+    rot.z = rotationMatrix(((y - ystart) * ppy + i) * pi * apy / 360, 0, 0, 1)
     rgl.viewpoint(userMatrix = rot.y %*% rot.z, fov=1, zoom=0.75)
-    ## fname = sprintf("movie%08d.png", (y - ystart) * 50 + i)
-    ## rgl.snapshot(file.path(md, fname))
-    ## system(paste0("mogrify -fill black -pointsize 36 -annotate +50+68 '", location, " ", y, "' ", file.path(md, fname)))
+    ## save the image and add a label
+    fname = sprintf("movie%08d.png", (y - ystart) * ppy + i)
+    rgl.snapshot(file.path(md, fname))
+    system(paste0("mogrify -fill black -pointsize 36 -annotate +50+68 '", location, " ", y, "' ", file.path(md, fname)))
   }
 }
-## owd = getwd()
-## setwd(md)
-## system("ffmpeg -y -f image2 -r 25 -i movie%08d.png -vcodec libx264 movie.mp4")
-## setwd(owd)
+## convert the images to a movie
+owd = getwd()
+setwd(md)
+system("ffmpeg -y -f image2 -r 25 -i movie%08d.png -vcodec libx264 -pix_fmt yuv420p movie.mp4")
+setwd(owd)
--- demo/dgvm3d.ggsuccession.R
+++ demo/dgvm3d.ggsuccession.R
@@ -4,7 +4,7 @@ if (!require(data.table) || !require(ggplot2)) {
   warning("You need the packages 'data.table' and 'ggplot2' installed, to run this demo!")
 } else {
   location <- 'Canada - boreal forest'
-  dt <- data.table(dgvm3d.succession[[location]])
+  dt  <- data.table(dgvm3d.succession[[location]])
   lon <- unique(dt$Lon)
   lat <- unique(dt$Lat)
   ## mean over individuums
@@ -13,20 +13,24 @@ if (!require(data.table) || !require(ggplot2)) {
   dt = dt[, list(LAI=sum(LAI, na.rm=TRUE)), by=c("Year", "PID", "Pft")]
   ## fill missing values with zeros
   full.dt <- expand.grid(Year=unique(dt$Year), PID=unique(dt$PID), Pft=unique(dt$Pft))
-  dt = merge(dt, full.dt, by=c("Year" , "PID", "Pft"), all = TRUE)
-  dt[is.na(LAI), LAI:=0]
+  dt = merge(dt, full.dt, by=c("Year" , "PID", "Pft"), all=TRUE)
+  dt[is.na(LAI), LAI := 0]
+  ## plotting per patch
   gg <- ggplot(dt, aes(Year, LAI, group=Pft, fill=Pft))
   gg = gg + geom_area(position="stack")
   gg = gg + facet_wrap(~PID, ncol=2)
   gg = gg + labs(title=paste0(location, " (", lon, "/", lat, ")"))
   print(gg)
 
+  ## averaging over the patches
   dt = dt[, list(LAI=mean(LAI)), by=c("Year", "Pft")]
+  ## plotting the mean LAI value
   gg <- ggplot(dt, aes(Year, LAI, group=Pft, fill=Pft))
   gg = gg + geom_area(position="stack")
   gg = gg + labs(title=paste0(location, " (", lon, "/", lat, ")"))
   print(gg)
 
+  ## Same for another grassy location
   location <- "Africa - sahel"
   dt <- data.table(dgvm3d.succession[[location]])
   lon <- unique(dt$Lon)
@@ -37,8 +41,9 @@ if (!require(data.table) || !require(ggplot2)) {
   dt = dt[, list(LAI=sum(LAI, na.rm=TRUE)), by=c("Year", "PID", "Pft")]
   ## fill missing values with zeros
   full.dt = expand.grid(Year=unique(dt$Year), PID=unique(dt$PID), Pft=unique(dt$Pft))
-  dt = merge(dt, full.dt, by=c("Year" , "PID", "Pft"), all = TRUE)
+  dt = merge(dt, full.dt, by=c("Year" , "PID", "Pft"), all=TRUE)
   dt[is.na(LAI), LAI:=0]
+  ## only stand average over patches
   dt = dt[, list(LAI=mean(LAI)), by=c("Year", "Pft")]
   gg <- ggplot(dt, aes(Year, LAI, group=Pft, fill=Pft))
   gg = gg + geom_area(position="stack")
