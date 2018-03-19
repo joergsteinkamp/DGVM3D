c2befa313cb6b268415c706db5bdfd710cf2f412
Joerg Steinkamp <joerg.steinkamp@senckenberg.de>
2018-03-19 08:08:30 +0100

    Copied DESCRIPTION text to package man page

--- R/DGVM3D.R
+++ R/DGVM3D.R
@@ -1,9 +1,12 @@
-#' 3D Visualization of simulated vegetation structure
+#' 3D Forest Simulation Visualization Tool
 #'
-#' This is a visualization tool for simulated vegetation structure/succession in space and/or time of forest gap models. Those models should contain either individual trees or cohorts (e.g. LPJ-GUESS: Smith et al. (2001, 2014)).
+#' This is a visualization tool for vegetation structure/succession
+#' in space and/or time mainly for forest gap models. However, it could also be used to
+#' visualize observed forest stands. If used for models, they should contain either
+#' individual trees or cohorts (e.g. LPJ-GUESS by Smith et al. (2014) <doi:10.5194/bg-11-2027-2014>).
+#' For a list of required and additional data fileds see the vignette.
 #'
 #' @references
-#' Smith, B., Prentice, I. C. and Sykes, M. T.: Representation of vegetation dynamics in the modelling of terrestrial ecosystems: comparing two contrasting approaches within European climate space, Global Ecology and Biogeography, 10(6), 621-637, \href{https://dx.doi.org/10.1046/j.1466-822X.2001.t01-1-00256.x}{doi:10.1046/j.1466-822X.2001.t01-1-00256.x}, 2001.
 #' Smith, B., Warlind, D., Arneth, A., Hickler, T., Leadley, P., Siltberg, J. and Zaehle, S.: Implications of incorporating N cycling and N limitations on primary production in an individual-based dynamic vegetation model, Biogeosciences, 11(7), 2027-2054, \href{https://dx.doi.org/10.5194/bg-11-2027-2014}{doi:10.5194/bg-11-2027-2014}, 2014.
 #'
 "_PACKAGE"
